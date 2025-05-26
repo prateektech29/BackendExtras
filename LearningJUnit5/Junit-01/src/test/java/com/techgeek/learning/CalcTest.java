d3bc0f987dc1c7e15f14e4af9017c75d0eb5fba2
prateektech29 <prateektech29@gmail.com>
2025-05-26 12:33:49 +0530

    test(junit): add initial  JUnit practice program

---
+++ LearningJUnit5/Junit-01/src/test/java/com/techgeek/learning/CalcTest.java
@@ -0,0 +1,17 @@
+package com.techgeek.learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void test() {
+		Calc c=new Calc();
+		int actual=c.divide(10, 5);
+		int expectedResult=2;
+		assertEquals(expectedResult, actual);
+	}
+
+}
